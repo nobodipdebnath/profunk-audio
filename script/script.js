e81abc68aa8f7e20d1940a15c01746c1551dfaa0
Nobodip Debnath <nobodipdebnath45@gmail.com>
2025-11-17 11:47:26 +0600

    add tablat device

--- script/script.js
+++ script/script.js
@@ -1,13 +1,12 @@
 const menuBtn = document.getElementById('mobile-menu');
 const links = document.getElementById('links');
 
-// Toggle Show/Hide
 menuBtn.addEventListener('click', (e) => {
-    e.stopPropagation(); // বাইরে ক্লিক ধরতে সমস্যা না হয়
+    e.stopPropagation();
     links.classList.toggle('active');
 });
 
-// বাইরে ক্লিক করলে Hide হবে
+
 document.addEventListener('click', (e) => {
     if (!links.contains(e.target) && e.target !== menuBtn) {
         links.classList.remove('active');
